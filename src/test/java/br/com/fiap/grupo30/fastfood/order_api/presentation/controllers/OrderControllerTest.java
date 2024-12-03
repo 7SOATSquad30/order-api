2bd432b59f41e8291b7f6a443c10babdfad00350
Gabriel De Gennaro <gabrieldgennaro@gmail.com>
2024-12-02 23:08:21 -0300

    Add more tests

--- src/test/java/br/com/fiap/grupo30/fastfood/order_api/presentation/controllers/OrderControllerTest.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/order_api/presentation/controllers/OrderControllerTest.java
@@ -7,20 +7,27 @@ import static org.mockito.Mockito.*;
 import br.com.fiap.grupo30.fastfood.order_api.domain.OrderStatus;
 import br.com.fiap.grupo30.fastfood.order_api.domain.PaymentStatus;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.customer.CustomerUseCase;
+import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.AddProductToOrderUseCase;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.DeliverOrderUseCase;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.FinishPreparingOrderUseCase;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.GetOrderUseCase;
+import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.ListOrdersByStatusUseCase;
+import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.ListOrdersWithSpecificStatusesUseCase;
+import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.RemoveProductFromOrderUseCase;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.StartNewOrderUseCase;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.StartPreparingOrderUseCase;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.SubmitOrderUseCase;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.product.*;
 import br.com.fiap.grupo30.fastfood.order_api.infrastructure.gateways.OrderGateway;
+import br.com.fiap.grupo30.fastfood.order_api.presentation.presenters.dto.AddOrderProductRequest;
 import br.com.fiap.grupo30.fastfood.order_api.presentation.presenters.dto.OrderDTO;
 import br.com.fiap.grupo30.fastfood.order_api.presentation.presenters.dto.OrderItemDTO;
 import br.com.fiap.grupo30.fastfood.order_api.presentation.presenters.dto.PaymentDTO;
 import br.com.fiap.grupo30.fastfood.order_api.utils.CustomerHelper;
 import br.com.fiap.grupo30.fastfood.order_api.utils.OrderHelper;
 import br.com.fiap.grupo30.fastfood.order_api.utils.ProductHelper;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -39,6 +46,10 @@ public class OrderControllerTest {
     @Mock private DeliverOrderUseCase deliverOrderUseCase;
     @Mock private FinishPreparingOrderUseCase finishPreparingOrderUseCase;
     @Mock private SubmitOrderUseCase submitOrderUseCase;
+    @Mock private AddProductToOrderUseCase addProductToOrderUseCase;
+    @Mock private RemoveProductFromOrderUseCase removeProductFromOrderUseCase;
+    @Mock private ListOrdersWithSpecificStatusesUseCase listOrdersWithSpecificStatusesUseCase;
+    @Mock private ListOrdersByStatusUseCase listOrdersByStatusUseCase;
 
     @InjectMocks private OrderController orderController;
 
@@ -66,6 +77,48 @@ public class OrderControllerTest {
 
             // Verify
             verify(getOrderUseCase, times(1)).execute(any(OrderGateway.class), eq(1L));
+        }
+
+        @Test
+        void shouldListOrdersWithSpecificStatusesUseCase() throws Exception {
+            Long orderId = 1L;
+            OrderDTO orderDTO =
+                    OrderHelper.createDefaultOrderDTOWithId(
+                            1L, CustomerHelper.valid(), ProductHelper.validProduct());
+            when(submitOrderUseCase.execute(any(OrderGateway.class), eq(orderId)))
+                    .thenReturn(orderDTO);
+
+            orderController.submitOrder(orderId);
+            // Arrange
+            orderController.findOrdersWithSpecificStatuses();
+
+            // Verify
+            verify(listOrdersWithSpecificStatusesUseCase, times(1))
+                    .execute(any(OrderGateway.class));
+        }
+
+        @Test
+        void shouldListAllOrdersByStatus() throws Exception {
+            // Arrange
+            String status = "DRAFT";
+            OrderDTO orderDTO =
+                    OrderHelper.createDefaultOrderDTOWithId(
+                            1L, CustomerHelper.valid(), ProductHelper.validProduct());
+            OrderDTO orderDTO2 =
+                    OrderHelper.createDefaultOrderDTOWithId(
+                            2L, CustomerHelper.valid(), ProductHelper.validProduct());
+            List<OrderDTO> listOrderDTO = new ArrayList<>();
+            listOrderDTO.add(orderDTO);
+            listOrderDTO.add(orderDTO2);
+
+            when(listOrdersByStatusUseCase.execute(any(OrderGateway.class), eq(status)))
+                    .thenReturn(listOrderDTO);
+
+            orderController.findOrdersByStatus(status);
+
+            // Verify
+            verify(listOrdersByStatusUseCase, times(1))
+                    .execute(any(OrderGateway.class), eq(status));
         }
     }
 
@@ -107,6 +160,78 @@ public class OrderControllerTest {
 
             // Verify
             verify(getOrderUseCase, times(1)).execute(any(OrderGateway.class), eq(1L));
+        }
+
+        @Test
+        void shouldInvokeAddProductToOrderUseCase() throws Exception {
+            // Arrange
+            Long orderId = 1L;
+            Long productId = 1L;
+            Long productQuantity = 2L;
+            OrderDTO orderDTO =
+                    OrderHelper.createDefaultOrderDTOWithId(
+                            1L, CustomerHelper.valid(), ProductHelper.validProduct());
+            when(addProductToOrderUseCase.execute(
+                            any(OrderGateway.class),
+                            any(ProductUseCase.class),
+                            eq(orderId),
+                            eq(productId),
+                            eq(productQuantity)))
+                    .thenReturn(orderDTO);
+
+            orderController.addProduct(
+                    orderId, new AddOrderProductRequest(productId, productQuantity));
+
+            // Verify
+            verify(addProductToOrderUseCase, times(1))
+                    .execute(
+                            any(OrderGateway.class),
+                            any(ProductUseCase.class),
+                            eq(1L),
+                            eq(1L),
+                            eq(2L));
+        }
+
+        @Test
+        void shouldInvokeRemoveProductFromOrderUseCase() throws Exception {
+            // Arrange
+            Long orderId = 1L;
+            Long productId = 1L;
+            OrderDTO orderDTO =
+                    OrderHelper.createDefaultOrderDTOWithId(
+                            1L, CustomerHelper.valid(), ProductHelper.validProduct());
+            when(removeProductFromOrderUseCase.execute(
+                            any(OrderGateway.class),
+                            any(ProductUseCase.class),
+                            eq(orderId),
+                            eq(productId)))
+                    .thenReturn(orderDTO);
+
+            orderController.removeProduct(orderId, productId);
+
+            // Verify
+            verify(removeProductFromOrderUseCase, times(1))
+                    .execute(
+                            any(OrderGateway.class),
+                            any(ProductUseCase.class),
+                            eq(orderId),
+                            eq(productId));
+        }
+
+        @Test
+        void shouldInvokeSubmitOrderUseCase() throws Exception {
+            // Arrange
+            Long orderId = 1L;
+            OrderDTO orderDTO =
+                    OrderHelper.createDefaultOrderDTOWithId(
+                            1L, CustomerHelper.valid(), ProductHelper.validProduct());
+            when(submitOrderUseCase.execute(any(OrderGateway.class), eq(orderId)))
+                    .thenReturn(orderDTO);
+
+            orderController.submitOrder(orderId);
+
+            // Verify
+            verify(submitOrderUseCase, times(1)).execute(any(OrderGateway.class), eq(orderId));
         }
     }
 
