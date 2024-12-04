9b7ca4d57f7c4697f05df6020ab33d8d5dd27045
Murilo Kakazu <yuri.kakazu@gmail.com>
2024-12-03 23:56:57 -0300

    test: fix spec

--- src/test/java/br/com/fiap/grupo30/fastfood/order_api/domain/usecases/StartPreparingOrderUseCaseTest.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/order_api/domain/usecases/StartPreparingOrderUseCaseTest.java
@@ -5,19 +5,18 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import br.com.fiap.grupo30.fastfood.order_api.domain.OrderStatus;
+import br.com.fiap.grupo30.fastfood.order_api.domain.PaymentStatus;
 import br.com.fiap.grupo30.fastfood.order_api.domain.entities.Order;
-import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.customer.CustomerUseCase;
-import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.StartNewOrderUseCase;
+import br.com.fiap.grupo30.fastfood.order_api.domain.entities.Payment;
 import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.order.StartPreparingOrderUseCase;
+import br.com.fiap.grupo30.fastfood.order_api.domain.usecases.payment.PaymentUseCase;
 import br.com.fiap.grupo30.fastfood.order_api.infrastructure.gateways.OrderGateway;
 import br.com.fiap.grupo30.fastfood.order_api.infrastructure.persistence.repositories.JpaOrderRepository;
 import br.com.fiap.grupo30.fastfood.order_api.presentation.controllers.OrderController;
 import br.com.fiap.grupo30.fastfood.order_api.presentation.presenters.dto.OrderDTO;
-import br.com.fiap.grupo30.fastfood.order_api.utils.CustomerHelper;
 import br.com.fiap.grupo30.fastfood.order_api.utils.OrderHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.stereotype.Component;
@@ -26,36 +25,33 @@ import org.springframework.stereotype.Component;
 class StartPreparingOrderUseCaseTest {
 
     @Mock private OrderGateway orderGateway;
-    @Mock private CustomerUseCase customerUseCase;
+    @Mock private PaymentUseCase paymentUseCase;
     @Mock private JpaOrderRepository jpaOrderRepository;
     @Mock private OrderController orderController;
 
-    @InjectMocks private StartPreparingOrderUseCase startPreparingOrderUseCase;
-    @InjectMocks private StartNewOrderUseCase startNewOrderUseCase;
+    private StartPreparingOrderUseCase startPreparingOrderUseCase;
 
     private static final Long DEFAULT_ORDERID = 1L;
-    private static final String DEFAULT_CUSTOMER_CPF = "29375235017";
-    private static final OrderStatus DEFAULT_ORDERSTATUS = OrderStatus.PREPARING;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         startPreparingOrderUseCase = new StartPreparingOrderUseCase();
-        startNewOrderUseCase = new StartNewOrderUseCase();
     }
 
     @Test
     void shouldReturnOrderDTOWithCorrectStatus() {
         // Arrange
-        Order order = OrderHelper.createDefaultOrderStatus(DEFAULT_ORDERID, DEFAULT_ORDERSTATUS);
+        Order order = OrderHelper.createDefaultOrderStatus(DEFAULT_ORDERID, OrderStatus.SUBMITTED);
 
+        when(orderGateway.findById(order.getId())).thenReturn(order);
         when(orderGateway.save(any(Order.class))).thenReturn(order);
-        when(customerUseCase.findCustomerByCpf(any(String.class)))
-                .thenReturn(CustomerHelper.createDefaultCustomerWithId(order.getCustomerId()));
+        when(paymentUseCase.findPaymentStateByOrderId(order.getId()))
+                .thenReturn(Payment.create(PaymentStatus.COLLECTED, order.getTotalPrice()));
 
         // Act
         OrderDTO result =
-                startNewOrderUseCase.execute(orderGateway, customerUseCase, DEFAULT_CUSTOMER_CPF);
+                startPreparingOrderUseCase.execute(orderGateway, paymentUseCase, order.getId());
 
         // Assert
         assertThat(result.getStatus()).isEqualTo(order.getStatus());
@@ -64,15 +60,16 @@ class StartPreparingOrderUseCaseTest {
     @Test
     void shouldReturnOrderDTOWithCorrectCustomerName() {
         // Arrange
-        Order order = OrderHelper.createDefaultOrderStatus(DEFAULT_ORDERID, DEFAULT_ORDERSTATUS);
+        Order order = OrderHelper.createDefaultOrderStatus(DEFAULT_ORDERID, OrderStatus.SUBMITTED);
 
+        when(orderGateway.findById(order.getId())).thenReturn(order);
         when(orderGateway.save(any(Order.class))).thenReturn(order);
-        when(customerUseCase.findCustomerByCpf(any(String.class)))
-                .thenReturn(CustomerHelper.createDefaultCustomerWithId(order.getCustomerId()));
+        when(paymentUseCase.findPaymentStateByOrderId(order.getId()))
+                .thenReturn(Payment.create(PaymentStatus.COLLECTED, order.getTotalPrice()));
 
         // Act
         OrderDTO result =
-                startNewOrderUseCase.execute(orderGateway, customerUseCase, DEFAULT_CUSTOMER_CPF);
+                startPreparingOrderUseCase.execute(orderGateway, paymentUseCase, order.getId());
 
         // Assert
         assertThat(result.getCustomerId()).isEqualTo(order.getCustomerId());
